9851d709e0b79cbb36809f298f9c94bb8086fccc
truongkimson <sontks@gmail.com>
2021-07-16 22:43:51 +0800

    use useContext instead of useState

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,47 +1,64 @@
-import React from 'react'
-import { useState } from 'react'
-import Button from './Button'
-import FormBody from './FormBody'
-import { FaRegCalendar, FaTimes } from 'react-icons/fa'
+import React from "react";
+import { useContext } from "react";
+import Button from "./Button";
+import FormBody from "./FormBody";
+import { FaRegCalendar, FaTimes } from "react-icons/fa";
+import { CurrentUpdateContext } from "./TaskList";
 
-const Task = ({ task, onDeleteTask, onUpdateTask }) => {
-    const [isUpdating, setIsUpdating] = useState(false)
+const Task = ({ task, onDeleteTask, onUpdateTask, onUpdateChange }) => {
+  // const [isUpdating, setIsUpdating] = useState(false);
+  const currentUpdate = useContext(CurrentUpdateContext)
 
-    return (
-        <div className="card my-1">
-            {isUpdating ?
-                <>
-                    <div>
-                        <FaTimes className="me-3 mt-3 float-end" size={20}
-                            style={{ cursor: 'pointer' }} onClick={() => setIsUpdating(false)} />
-                    </div>
+  return (
+    <div className="card my-1">
+      {currentUpdate === task.id ? (
+        <>
+          <div>
+            <FaTimes
+              className="me-3 mt-3 float-end"
+              size={20}
+              style={{ cursor: "pointer" }}
+              onClick={() => onUpdateChange(-1)}
+            />
+          </div>
 
-                    <FormBody onAction={onUpdateTask} action="Update"
-                        defaultTask={task} onComplete={() => setIsUpdating(false)} />
-                </>
-                : <div className="card-body">
-                    <div className="row">
-                        <div className="col-10">
-                            <h5 onClick={() => setIsUpdating(true)} style={{ cursor: 'pointer' }}>
-                                {task.description}
-                            </h5>
-                            <div>
-                                <FaRegCalendar className="me-1 align-baseline" />
-                                {task.date}
-                            </div>
-                            {task.overdue && <div>
-                                <span className="text-danger">OVERDUE</span>
-                            </div>}
-                        </div>
-                        <div className="col-2">
-                            <div className="text-center">
-                                <Button text="Done" onClick={() => onDeleteTask(task.id)} />
-                            </div>
-                        </div>
-                    </div>
-                </div>}
+          <FormBody
+            onAction={onUpdateTask}
+            action="Update"
+            defaultTask={task}
+            onComplete={() => onUpdateChange(-1)}
+          />
+        </>
+      ) : (
+        <div className="card-body">
+          <div className="row">
+            <div className="col-10">
+              <h5
+                onClick={() => onUpdateChange(task.id)}
+                style={{ cursor: "pointer" }}
+              >
+                {task.description}
+              </h5>
+              <div>
+                <FaRegCalendar className="me-1 align-baseline" />
+                {task.date}
+              </div>
+              {task.overdue && (
+                <div>
+                  <span className="text-danger">OVERDUE</span>
+                </div>
+              )}
+            </div>
+            <div className="col-2">
+              <div className="text-center">
+                <Button text="Done" onClick={() => onDeleteTask(task.id)} />
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Task
+export default Task;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import Task from './Task'
+import React from "react";
+import { useState } from "react"
+import Task from "./Task";
+
+export const CurrentUpdateContext = React.createContext();
 
 const TaskList = ({ tasks, onDeleteTask, onUpdateTask }) => {
-    return (
-        <div className="container">
-            {tasks.map(task => (<Task key={task.id} task={task}
-                onDeleteTask={onDeleteTask} onUpdateTask={onUpdateTask} />))}
-        </div>
-    )
-}
+    const [ currentUpdate, setCurrentUpdate ] = useState(-1)
 
-export default TaskList
+    return (
+        <CurrentUpdateContext.Provider value={currentUpdate}>
+            <div className="container">
+                {tasks.map((task) => (
+                    <Task
+                        key={task.id}
+                        task={task}
+                        onDeleteTask={onDeleteTask}
+                        onUpdateTask={onUpdateTask}
+                        onUpdateChange={id => setCurrentUpdate(id)}
+                    />
+                ))}
+            </div>
+        </CurrentUpdateContext.Provider>
+    );
+};
+
+export default TaskList;
